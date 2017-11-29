c74aa53e85687434d6fb9a1a54e314d0bac561aa
Joseph Abell <joseph.abell@piksel.com>
2017-11-29 19:12:07 +0000

    Adding leaderboards

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,11 @@
 'use strict'
+
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  FIREBASE_API_KEY: "'" + process.env.npm_config_FIREBASE_API_KEY + "'",
+  FIREBASE_AUTH_DOMAIN: "'" + process.env.npm_config_FIREBASE_AUTH_DOMAIN + "'",
+  FIREBASE_DATABASE_URL: "'" + process.env.npm_config_FIREBASE_DATABASE_URL + "'",
+  FIREBASE_PROJECT_ID: "'" + process.env.npm_config_FIREBASE_PROJECT_ID + "'",
+  FIREBASE_STORAGE_BUCKET: "'" + process.env.npm_config_FIREBASE_STORAGE_BUCKET + "'",
+  FIREBASE_MESSAGING_SENDER_ID: "'" + process.env.npm_config_FIREBASE_MESSAGING_SENDER_ID + "'"
 }
--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import { db } from './firebase'
 
 Vue.config.productionTip = false
+console.log(db)
 
 /* eslint-disable no-new */
 new Vue({
